1e72a5a4c2e4263a0178c55d0402d407c09e9f5d
Thomas Lim <txl11@ic.ac.uk>
2012-12-09 19:08:36 +0000

    Worked on user class

--- class/user.class.php
+++ class/user.class.php
@@ -2,8 +2,10 @@
 
 class user
 {
-	function __construct($user = null)
+
+	function __construct($db ,$user = null)
 	{
+		$this->db = $db;
 		if ($user != null) //this is not checked for being correct - data taken from session
 		{
 			$this->authenticate($user);
@@ -12,13 +14,30 @@ class user
 
 	private function authenticate($username)
 	{
+        $escaped_user = $this->db->escape_string($username);
+        $result = $this->db->query();
 		$this->user = $username;
 		$this->usergroup = 2;
 	}
 
 	function login($username, $password) //this is the login. User can only exist if user exists..
 	{
-		if ($username == 'dm1911') //check the password is right (PAM)
+		if(!function_exists('pam_auth') && LOCAL) // here to simulate running on Dougal
+		{
+			function pam_auth($username, $password)
+			{
+				if ($username == 'dm1911' || $username == 'txl11')
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+
+		if (pam_auth($username, $password)) //check the password is right (pam_auth)
 		{
 			//check is the user has already completed?
 			$this->authenticate($username);
