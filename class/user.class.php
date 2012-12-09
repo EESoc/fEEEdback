fa1fbe3c2657e9017afb6f8361e86785e5c38a3b
Thomas Lim <txl11@ic.ac.uk>
2012-12-09 22:40:51 +0000

    Worked on user class. Started adding SQL

--- class/user.class.php
+++ class/user.class.php
@@ -1,5 +1,33 @@
 <?php
 
+if(!function_exists('pam_auth') && LOCAL) // here to simulate running on Dougal
+{
+    function pam_auth($username, $password)
+    {
+        if ($username == 'dm1911' || $username == 'txl11' || $username == 'troll')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+if(!function_exists('ldap_get_names') && LOCAL) // here to simulate running on Dougal
+{
+    function ldap_get_names($name)
+    {
+        $return = array(
+                                0 => "Love",
+                                1 => "Meister",
+                            );
+        return $return;
+    }
+}
+
+
 class user
 {
 
@@ -14,28 +42,34 @@ class user
 
 	private function authenticate($username)
 	{
-        $escaped_user = $this->db->escape_string($username);
-        $result = $this->db->query();
-		$this->user = $username;
-		$this->usergroup = 2;
+        //$username = $this->db->escape_string($username);
+        $result = $this->db->query("SELECT * FROM gta_users where uname = '$username'");
+        if($result->num_rows)
+        {
+            $row = $result->fetch_assoc();
+            if($row['completed'])
+            {
+                echo 'done survey';
+                return false; //Here if they have already completed the survey
+            }
+            echo 'ready to survey';
+            $this->usergroup = $row['labgroup'];        //We do this stuff now since they are allowed to do the survey
+            $this->user = $username;
+            $names = ldap_get_names($username);
+            $this->fname = $names[0];
+            return true;
+        }
+        else
+        {
+            echo 'not eligible to do survey';
+            return false; //Here because they aren't in the DB and hence not eligible to provide feedback.
+        }
+
 	}
 
 	function login($username, $password) //this is the login. User can only exist if user exists..
 	{
-		if(!function_exists('pam_auth') && LOCAL) // here to simulate running on Dougal
-		{
-			function pam_auth($username, $password)
-			{
-				if ($username == 'dm1911' || $username == 'txl11')
-				{
-					return true;
-				}
-				else
-				{
-					return false;
-				}
-			}
-		}
+
 
 		if (pam_auth($username, $password)) //check the password is right (pam_auth)
 		{
@@ -46,4 +80,14 @@ class user
 		
 		return false;
 	}
+
+    function completed_survey() //Sets the user as having completed the survey (when they are done). Should only be here if they have logged in, were eligible and submitted feedback.
+    {
+        $result = $this->db->query("UPDATE gta SET completed=1
+                                        WHERE uname='$this->user'");
+        if(!$result->num_rows)
+        {
+            echo 'Something went badly wrong here...';
+        }
+    }
 }
