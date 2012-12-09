edb4ddc30b8b0d45de1c082f774926ea2d41a778
Thomas Lim <txl11@ic.ac.uk>
2012-12-09 23:05:39 +0000

    Started work on the GTA class

---
+++ class/gta.class.php
@@ -0,0 +1,25 @@
+<?php
+
+class gta
+{
+    function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    function listgtas($group)
+    {
+        $result = $this->db->query("SELECT * FROM gta_gtas WHERE ee_group = '$group'");
+        if(!$result->num_rows)
+        {
+            echo 'Uh oh, this group doesn\'t appear to exist'; //Really we shouldn't have any undefined groups...
+            return;
+        }
+        $list = array();
+        while($row = $result->fetch_assoc())
+        {
+            array_push($list, $row["slug"]);
+        }
+
+    }
+}
