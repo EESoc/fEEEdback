40fe0393632583f2e4bccf18e35ecf25a349e225
Dario Magliocchetti <dario_dml@hotmail.com>
2013-09-21 19:04:30 +0100

    Updated structure of database

--- struct.sql
+++ struct.sql
@@ -57,3 +57,49 @@ CREATE TABLE IF NOT EXISTS `gta_users` (
   `completed` int(1) NOT NULL,
   PRIMARY KEY (`uname`,`labgroup`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gta_chem_feedback`
+--
+
+CREATE TABLE IF NOT EXISTS `gta_chem_feedback` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `gtaid` int(5) NOT NULL,
+  `uname` varchar(8) NOT NULL,
+  `vote` int(2) NOT NULL,
+  `comment` text NOT NULL,
+  `submit_time` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gta_chem_gtas`
+--
+
+CREATE TABLE IF NOT EXISTS `gta_chem_gtas` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `image` varchar(75) NOT NULL,
+  `group` varchar(30) NOT NULL,
+  `experiment` varchar(30) NOT NULL,
+  `open` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gta_chem_users`
+--
+
+CREATE TABLE IF NOT EXISTS `gta_chem_users` (
+  `uname` varchar(8) NOT NULL,
+  `labgroup` varchar(5) NOT NULL,
+  `completed` int(1) NOT NULL,
+  PRIMARY KEY (`uname`,`labgroup`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
